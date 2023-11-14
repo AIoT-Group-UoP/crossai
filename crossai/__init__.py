ab351dc98476099ba68256e7c48d757b88b81b40
Pantelis Tzamalis <tzamalis@ceid.upatras.gr>
2023-11-14 20:46:16 +0200

    setup release crossai v0.1

--- crossai/__init__.py
+++ crossai/__init__.py
@@ -5,7 +5,7 @@ An open-source library that consists of functionalities capable of processing
 and developing ML pipelines for Time Series Analysis.
 """
 
-__version__ = "0.0.2"
+__version__ = "0.1"
 __author__ = ("Pantelis Tzamalis, Panagiotis Kapetanidis, Fotios Kalioras, "
               "George Kontogiannis, Andreas Bardoutsos")
 __credits__ = ("Artificial Intelligence of Things Group (AIoT Group), "
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="crossai",
-    version="0.0.2",
+    version="0.1",
     packages=[
         "crossai",
         "crossai.ai",
