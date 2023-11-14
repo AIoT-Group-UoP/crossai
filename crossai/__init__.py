785b00a7e96e9d4d6a9e6dafe39bfc6928e382b8
Pantelis Tzamalis <tzamalis@ceid.upatras.gr>
2023-11-14 20:23:53 +0200

    add info to setup.py and the init for the lib information

--- crossai/__init__.py
+++ crossai/__init__.py
@@ -1,1 +1,12 @@
+"""
+CrossAI.
 
+An open-source library that consists of functionalities capable of processing
+and developing ML pipelines for Time Series Analysis.
+"""
+
+__version__ = "0.0.1"
+__author__ = ("Pantelis Tzamalis, Panagiotis Kapetanidis, Fotios Kalioras, "
+              "George Kontogiannis, Andreas Bardoutsos")
+__credits__ = ("Artificial Intelligence of Things Group (AIoT Group), "
+               "University of Patras, Greece")
--- setup.py
+++ setup.py
@@ -9,10 +9,18 @@ setup(
         "crossai.ai.nn1d",
         "crossai.ai.nn2d"
     ],
+    url="https://github.com/tzamalisp/crossai",
+    license="Apache License 2.0",
+    author="Pantelis Tzamalis, Panagiotis Kapetanidis, Fotios Kalioras, "
+           "George Kontogiannis, Andreas Bardoutsos",
+    author_email="tzamalis@ceid.upatras.gr",
+    description="An open-source library that consists of functionalities "
+                "capable of processing and developing ML pipelines for Time "
+                "Series Analysis",
     install_requires=[
         "tensorflow==2.13.0",
         'tensorflow-metal==1.0.0; platform_system=="Darwin"',
         "tensorflow_addons>=0.21.0"
     ],
-    python_requires=">=3.11.4"
+    python_requires=">=3.8"
 )
