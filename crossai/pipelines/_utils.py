6dfb336f4d5da000c2ee7caaa6d36a4afffb7271
Pantelis Tzamalis <tzamalis@ceid.upatras.gr>
2023-11-15 16:50:01 +0200

    delete duplicated class

--- crossai/pipelines/_utils.py
+++ crossai/pipelines/_utils.py
@@ -3,8 +3,8 @@ from sklearn.base import BaseEstimator, TransformerMixin
 
 
 class DebugTransformer(TransformerMixin):
-    """Transformer that prints the name of itself when called.
-    Usefull as an intermediate step in a pipeline to debug steps.
+    """Transformer that prints the name of itself when called. Useful as an
+        intermediate step in a pipeline to debug steps.
     """
     def __init__(self, name="DebugTransformer"):
         self.name = name
@@ -19,36 +19,8 @@ class DebugTransformer(TransformerMixin):
 
 
 class TimeMeasurementWrapper(BaseEstimator, TransformerMixin):
-    """
-    Wrapper for sklearn transformers that prints the time a transformer
-    takes to fit and transform.
-    """
-    def __init__(self, transformer: TransformerMixin):
-        self.transformer = transformer
-
-    def fit(self, X, y=None):
-        start_time = time.time()
-        self.transformer.fit(X, y)
-        end_time = time.time()
-        execution_time = end_time - start_time
-        print(f"Transformer {type(self.transformer).__name__} took "
-              f"{execution_time:.2f} seconds to fit.")
-        return self
-
-    def transform(self, X):
-        start_time = time.time()
-        transformed_X = self.transformer.transform(X)
-        end_time = time.time()
-        execution_time = end_time - start_time
-        print(f"Transformer {type(self.transformer).__name__} took "
-              f"{execution_time:.2f} seconds to transform.")
-        return transformed_X
-
-
-class TimeMeasurementWrapper(BaseEstimator, TransformerMixin):
-    """
-    Wrapper for sklearn transformers that prints the time a transformer
-    takes to fit and transform.
+    """Wrapper for sklearn transformers that prints the time a transformer
+        takes to fit and transform.
     """
     def __init__(self, transformer: TransformerMixin):
         self.transformer = transformer
@@ -60,6 +32,7 @@ class TimeMeasurementWrapper(BaseEstimator, TransformerMixin):
         execution_time = end_time - start_time
         print(f"Transformer {type(self.transformer).__name__} took "
               f"{execution_time:.2f} seconds to fit.")
+
         return self
 
     def transform(self, X):
@@ -69,4 +42,5 @@ class TimeMeasurementWrapper(BaseEstimator, TransformerMixin):
         execution_time = end_time - start_time
         print(f"Transformer {type(self.transformer).__name__} took "
               f"{execution_time:.2f} seconds to transform.")
+
         return transformed_X
