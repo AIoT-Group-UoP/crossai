02e1969ac6e7b3748efd5a9db875f7c50b6fce41
CaptainAlready <pkapetanidis@gmail.com>
2023-11-15 12:43:20 +0200

    add spec_augment

--- crossai/processing/audio/augment.py
+++ crossai/processing/audio/augment.py
@@ -1,3 +1,4 @@
+import random
 from random import randint
 import numpy as np
 
@@ -19,3 +20,46 @@ def roll_signal(sig, augment_times, n_roll=randint(10000, 50000)):
         rolled_sig.append(np.roll(sig, n_roll))
         n_roll = randint(20000, 50000)
     return rolled_sig
+
+
+def spec_augment(spectrogram, augment_times=1, masks=2, freq_masking=0.15, time_masking=0.15):
+    """
+    Implementation of SpecAugment using numpy.
+
+    Args:
+        spectrogram (numpy.ndarray): Input 2D spectrogram with shape (freq, time).
+        augment_times (int): Number of times to augment original spectrogram.
+        masks (int): Number of masks for frequency and time masking.
+        freq_masking (float, optional): Maximum frequency masking length. Defaults to 0.15.
+        time_masking (float, optional): Maximum time masking length. Defaults to 0.15.
+
+    Returns:
+        augmented_spectrograms (numpy.ndarray): List of augmented spectrograms.
+    """
+
+    augmented_spectrograms = []
+    original_spectrogram = spectrogram.copy()
+    masks = max(1, masks)  # mask cannot be 0 or negative
+
+    for i in range(augment_times):
+        augmented = original_spectrogram.copy()
+
+        for i in range(masks):
+
+            # frequency masking
+            freqs, time_frames = augmented.shape
+            freq_mask_percentage = random.uniform(0.0, freq_masking)
+            masked_freqs = int(freq_mask_percentage * freqs)
+            f0 = int(np.random.uniform(low=0.0, high=freqs - masked_freqs))
+            augmented[f0:f0 + masked_freqs, :] = spectrogram.min()
+
+            # time masking
+            time_frames_mask_percentage = random.uniform(0.0, time_masking)
+            masked_time_frames = int(time_frames_mask_percentage * time_frames)
+            t0 = int(np.random.uniform(
+                low=0.0, high=time_frames - masked_time_frames))
+            augmented[:, t0:t0 + masked_time_frames] = spectrogram.min()
+
+        augmented_spectrograms.append(augmented)
+
+    return augmented_spectrograms
