239a74e9eec594884c525abbb3472c5b9c7ca200
CaptainAlready <pkapetanidis@gmail.com>
2023-11-15 13:10:03 +0200

    add q_transform

--- crossai/processing/audio/transform.py
+++ crossai/processing/audio/transform.py
@@ -74,3 +74,52 @@ def spectrum_calibration(target: str, reference: str, sr: float, config: Config 
             correct_result = correct_result.mean(axis=1)
 
         return correct_result
+
+
+def q_transform(sig, sr=44100, n_bins=84, hop_length=512, fmin=55, norm=1,
+                bins_per_octave=12, tuning=None, filter_scale=1, sparsity=0.01,
+                window='hann', scale=True, pad_mode='reflect', to_dB=True,
+                dsize=None):
+    """
+    Compute the constant-Q transform of an audio signal.
+
+    Args:
+        sig (numpy array): Input signal
+        sr (int): Sampling rate of the input signal
+        n_bins (int): Number of frequency bins
+        hop_length (int): Number of samples between successive frames
+        fmin (float): Minimum frequency
+        norm (int): Normalization factor
+        bins_per_octave (int): Number of bins per octave
+        tuning (float): Deviation from A440 tuning in fractional bins
+        filter_scale (float): Filter scale factor. Small values (<1) use
+                                shorter windows for improved time resolution.
+        sparsity (float): Sparsity of the CQT basis
+        window (string): Type of window to use
+        scale (bool): If True, scale the magnitude of the CQT by n_bins
+        pad_mode (string): If center=True, the padding mode to use at the
+                            edges of the signal. 
+                                By default, STFT uses reflection padding.
+        to_dB (bool): Convert the spectrogram to dB scale
+        dsize (tuple): Size of the output spectrogram : if None, the output is the raw spectrogram 
+
+    Returns:
+        q_transform (numpy array): Returns the constant-Q transform of an
+                                    audio signal
+    """
+
+    q_transform = librosa.core.cqt(sig, sr=sr, n_bins=n_bins,
+                                   hop_length=hop_length, fmin=fmin,
+                                   norm=norm, bins_per_octave=bins_per_octave,
+                                   tuning=tuning, filter_scale=filter_scale,
+                                   sparsity=sparsity, window=window,
+                                   scale=scale, pad_mode=pad_mode)
+
+    if to_dB is True:
+        q_transform = librosa.amplitude_to_db(q_transform, ref=np.max)
+
+    if dsize is not None:
+        q_transform = cv2.resize(
+            q_transform, dsize=dsize, interpolation=cv2.INTER_CUBIC)
+
+    return q_transform
