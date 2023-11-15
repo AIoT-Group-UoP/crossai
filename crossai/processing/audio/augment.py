45d74569d7878114fef23af6cabb773515890e50
CaptainAlready <pkapetanidis@gmail.com>
2023-11-15 13:05:47 +0200

    add time stretch augmentation

--- crossai/processing/audio/augment.py
+++ crossai/processing/audio/augment.py
@@ -23,3 +23,22 @@ def loudness(sig, augment_times, factor_low, factor_high):
         aug = naa.LoudnessAug(factor=factor)
         loud_sig.append(np.array(aug.augment(sig)).flatten())
     return loud_sig
+
+
+def time_stretch(signal, augment_times, factor):
+    """
+    Change the speed of the signal
+
+    Args:
+        sig (numpy array): Input signal
+        sr (int): Sampling rate of the input signal
+        factor (float): Factor to change the speed of the signal
+
+    Returns:
+        stretched_sig (numpy array): Returns the speed changed signal
+    """
+    stretched_signal = []
+    for i in range(augment_times):
+        stretched_signal.append(librosa.effects.time_stretch(signal, factor))
+        factor = np.random.uniform(0.1, 1)
+    return stretched_signal
