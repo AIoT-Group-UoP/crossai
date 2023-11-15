d4527aa37fd391f4557c7f0379a7b2bf653c2ab3
fothot2 <fkalioras@gmail.com>
2023-11-15 11:43:35 +0000

    Style changes in preparation

--- crossai/pipelines/preparation.py
+++ crossai/pipelines/preparation.py
@@ -1,21 +1,18 @@
 import numpy as np
-import warnings
 import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
 from crossai.pipelines.timeseries import TimeSeries
 
 
 class Augmenter(BaseEstimator, TransformerMixin):
-    """
-    Class for custom augmenter creation.
+    """Class for custom augmenter creation.
 
     Args:
-        func (function): Function to augment the data
-        **kwargs: Keyword arguments for the augmenter function
+        func (function): Function to augment the data.
+        **kwargs: Keyword arguments for the augmenter function.
 
     Returns:
         Augmenter object implementing the custom function on the data.
-
     """
 
     def __init__(self, func, **kwargs):
@@ -39,16 +36,15 @@ class Augmenter(BaseEstimator, TransformerMixin):
 
 
 def augment_signal_data(crossai_object, augs):
-    """
-    Augment the data using the augmenters
+    """Augment the data using the augmenters.
 
     Args:
-        crossai_object (CrossAI_Audio): CrossAI_Audio object
-        augs (list): List of augmenters
+        crossai_object (CrossAI_Audio): CrossAI_Audio object.
+        augs (list): List of augmenters.
 
     Returns:
         crossai_object (CrossAI_Audio): CrossAI_Audio object with augmented
-                                            data
+            data.
     """
 
     df_aug = pd.DataFrame(columns=['data', 'label'])
@@ -66,30 +62,20 @@ def augment_signal_data(crossai_object, augs):
 
 
 class Scaler(BaseEstimator, TransformerMixin):
-    """
-    Create a transformer class for the pipeline using a custom function
+    """Create a transformer class for the pipeline using a custom function
 
     Args:
-        func (function): Function to be used in the pipeline
-        **kwargs: Keyword arguments to be passed to the function
+        func (function): Function to be used in the pipeline.
+        **kwargs: Keyword arguments to be passed to the function.
     """
 
     def __init__(self, scaler):
-        """
-        Initialize the Transformer class
-        """
         self.scaler = scaler
 
     def fit(self, X, y=None):
-        """
-        Fit the transformer
-        """
         return self
 
     def transform(self, X):
-        """
-        Transform the data using the custom function
-        """
 
         X.data = self.scaler.fit_transform(X.data)
 
