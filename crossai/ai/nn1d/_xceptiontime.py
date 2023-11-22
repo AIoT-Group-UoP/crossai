7d3ead462850c3498530859108b15b4003d37d57
gkontogiannhs <g.kontogiannis@upnet.gr>
2023-11-22 16:44:50 +0200

    pep8 and docstring changes

--- crossai/ai/nn1d/_xceptiontime.py
+++ crossai/ai/nn1d/_xceptiontime.py
@@ -1,4 +1,4 @@
-import logging
+import tensorflow as tf
 from tensorflow.keras.layers import Input, Dense, Conv1D, MaxPooling1D
 from tensorflow.keras.layers import SeparableConv1D, Flatten, Add, Concatenate
 from tensorflow.keras.layers import BatchNormalization, Activation
@@ -12,22 +12,27 @@ import tensorflow_addons as tfa
 # Implementation of XceptionTime NN model based on:
 # - https://arxiv.org/pdf/1911.03803.pdf
 # - https://ieeexplore.ieee.org/document/9881698/
-def XceptionTime(input_shape,
-                 include_top=True,
-                 num_classes=1,
-                 classifier_activation='softmax',
-                 xception_adaptive_size=50,
-                 xception_adapt_ws_divide=4,
-                 n_filters=16,
-                 kernel_initialize="he_uniform",
-                 kernel_regularize=1e-5,
-                 kernel_constraint=3,
-                 drp_input=0,
-                 drp_mid=0,
-                 drp_high=0,
-                 spatial=False,
-                 mc_inference=None):
-    """A novel deep learning model referred to as the XceptionTime
+def XceptionTime(
+    input_shape: tuple,
+    include_top: bool = True,
+    num_classes: int = 1,
+    classifier_activation: str = "softmax",
+    xception_adaptive_size: int = 50,
+    xception_adapt_ws_divide: int = 4,
+    n_filters: int = 16,
+    drp_low: float = 0.,
+    drp_mid: float = 0.,
+    drp_high: float = 0.,
+    spatial: bool = False,
+    mc_inference: bool = None,
+    kernel_initialize: str = "he_uniform",
+    kernel_regularize: float = 1e-5,
+    kernel_constraint: int = 3
+) -> tf.keras.models.Model:
+    """Constructs a deep neural network based on Xception Architecture meant
+        for Time-Series tasks.
+
+    A novel deep learning model referred to as the XceptionTime
     architecture. The proposed innovative XceptionTime is designed by the
     integration of depthwise separable convolutions, adaptive average pooling,
     and a novel non-linear normalization technique. By utilizing the depthwise
@@ -39,44 +44,31 @@ def XceptionTime(input_shape,
     generalization.
 
     Args:
-        input_shape (tuple): The shape of a single instance of the dataset.
-        include_top (bool): whether to include a fully-connected layer at the
-        top of the network.
-        num_classes (int): number of classes to predict. Default 1.
-        classifier_activation (tf.keras.activation): activation function
-            (either as str or object) for the classification task.
-        xception_adaptive_size (int): The adaptive size. Default: 50.
-        xception_adapt_ws_divide (int): The number that will divide the
-            adaptive size. Default 4.
-        n_filters (int): The number of filters. Default: 16
-        kernel_initialize (str): The variance scaling initializer.
-            Default value: "he_uniform".
-        kernel_regularize (float or str): Regularizer to apply a penalty on
-            the layer's kernel. Can be float or str in 1e-5 format.
-        kernel_constraint (int): The constraint of the value of the incoming
-            weights. Default 3.
-        drp_input (float): Fraction of the input units to drop in the input
-            dropout layer. Default: 0.
-        drp_mid (float): Fraction of the input units to drop in the
-            mid-dropout layer. Default: 0.
-        drp_high (float): Fraction of the input units to drop in the last
-            dropout layer. Default: 0.
-        spatial (Boolean): Determines the type of Dropout. If True, it applies
-            SpatialDropout1D else Monte Carlo Dropout. Default: False.
-        mc_inference (bool, optional):
-            -If true, Dropout is enabled even during inference.
-            -If False, Dropout is neither enabled on training nor
-                during inference.
-            -If None, Dropout is enabled during training but not during
-                inference. Defaults to None.
+        input_shape: Shape of the input data, excluding the batch size.
+        include_top: If true, includes a fully-connected layer at the top of
+            the model. Set to False for a custom layer.
+        num_classes: Number of classes to predict.
+        classifier_activation: Activation function for the classification task.
+        xception_adaptive_size: The adaptive size.
+        xception_adapt_ws_divide: The number that will divide the adaptive
+            size.
+        n_filters: The number of filters.
+        drp_low: Fraction of the input units to drop in the input dropout
+            layer.
+        drp_mid: Fraction of the input units to drop in the mid-dropout layer.
+        drp_high: Fraction of the input units to drop in the last dropout
+            layer.
+        spatial: If true, applies Spatial Dropout, else applies standard
+            Dropout.
+        mc_inference: If True, enables Monte Carlo dropout
+            during inference. Defaults to None.
+        kernel_initialize: The variance scaling initializer.
+        kernel_regularize: Penalty to apply on the layer's kernel.
+        kernel_constraint: The constraint of the value of the incoming weights.
 
     Returns:
-        A Keras Model instance.
+        model: A Keras Model instance.
     """
-
-    # confirm that things are working as expected
-    logging.info(input_shape[0])
-    logging.info(xception_adapt_ws_divide)
 
     # check and adjust adaptive size based on input shape
     if input_shape[0] % xception_adapt_ws_divide == 0:
@@ -102,7 +94,7 @@ def XceptionTime(input_shape,
     input_layer = Input(shape=input_shape, name="input_layer")
 
     # Dropout
-    x = dropout_layer_1d(inputs=input_layer, drp_rate=drp_input,
+    x = dropout_layer_1d(inputs=input_layer, drp_rate=drp_low,
                          spatial=spatial, mc_inference=mc_inference)
 
     # COMPONENT 1 - Xception Block
@@ -165,45 +157,52 @@ def XceptionTime(input_shape,
     return model
 
 
-def conv1d_block(inputs, nf, ks=1, strd=1, pad="same", bias=False, bn=True,
-                 act=True, act_func="relu", drp_on=False, drp_rate=0.5,
-                 spatial=True, mc_inference=None, kernel_initialize=None,
-                 kernel_regularize=None, kernel_constraint=None):
-
+def conv1d_block(
+    inputs: tf.Tensor,
+    nf: int,
+    ks: int = 1,
+    strd: int = 1,
+    pad: str = "same",
+    bias: bool = False,
+    bn: bool = True,
+    act: bool = True,
+    act_func: str = "relu",
+    drp_on: bool = False,
+    drp_rate: float = 0.5,
+    spatial: bool = True,
+    mc_inference: bool = None,
+    kernel_initialize: str = None,
+    kernel_regularize: float = None,
+    kernel_constraint: int = None
+):
     """Create a block of layers consisting of Conv1D, BatchNormalization,
         Activation and Dropout.
 
     Args:
-        inputs (tensor): Input tensor to the block.
-        nf (int): Number of filters for the Conv1D layer.
-        ks (int): Kernel size for the Conv1D layer. Defaults to 1.
-        strd (int): Strides for the Conv1D layer. Defaults to 1.
-        pad (str): Padding for the Conv1D layer. Defaults to "same".
-        bias (bool): Whether to use bias in Conv1D. Defaults to False.
-        bn (bool): Whether to use BatchNormalization. Defaults to True.
-        act (bool): Whether to use an Activation layer. Defaults to True.
-        act_func (str): Activation function to use. Defaults to "relu".
-        drp_on (bool): Whether to include a Dropout layer. Defaults to False.
-        drp_rate (float): Dropout rate. Defaults to 0.5.
-        spatial (bool): Whether to use spatial dropout. Defaults to True.
-        mc_inference (bool):
-            -If true, Dropout is enabled even during inference.
-            -If False, Dropout is neither enabled on training nor
-                during inference.
-            -If None, Dropout is enabled during training but not during
-                inference. Defaults to None.
-        kernel_initialize (str): Initialization for Conv1D layer.
-            Defaults to None.
-        kernel_regularize (str): Regularization for Conv1D layer.
-            Defaults to None.
-        kernel_constraint (str): Constraint for Conv1D layer.
-            Defaults to None.
+        inputs: Input tensor to the block.
+        nf: Number of filters for the Conv1D layer.
+        ks: Kernel size for the Conv1D layer.
+        strd: Strides for the Conv1D layer.
+        pad: Padding for the Conv1D layer.
+        bias: Whether to use bias in Conv1D.
+        bn: Whether to use BatchNormalization.
+        act: Whether to use an Activation layer.
+        act_func: Activation function to use.
+        drp_on: Whether to include a Dropout layer.
+        drp_rate: Dropout rate.
+        spatial: If true, applies Spatial Dropout, else applies standard
+            Dropout.
+        mc_inference: If True, enables Monte Carlo dropout
+            during inference. Defaults to None.
+        kernel_initialize: The variance scaling initializer.
+        kernel_regularize: Penalty to apply on the layer's kernel.
+        kernel_constraint: The constraint of the value of the incoming weights.
 
     Returns:
         tensor: Output tensor after applying the block of layers.
     """
 
-    x = Conv1D(filters=int(nf),
+    x = Conv1D(filters=nf,
                kernel_size=ks,
                strides=strd,
                padding=pad,
@@ -225,25 +224,29 @@ def conv1d_block(inputs, nf, ks=1, strd=1, pad="same", bias=False, bn=True,
     return x
 
 
-def xception_block(inputs, n_filters, depth=4, use_residual=True,
-                   kernel_initialize=None, kernel_regularize=None,
-                   kernel_constraint=None):
-
+def xception_block(
+    inputs: tf.Tensor,
+    n_filters: int,
+    depth: int = 4,
+    use_residual: bool = True,
+    kernel_initialize: bool = None,
+    kernel_regularize: bool = None,
+    kernel_constraint: bool = None
+):
     """Xception Block function that applies a series of Xception modules,
         potentially with residual connections.
 
     Args:
-        inputs (tensor): Input tensor to the block.
-        n_filters (int): Number of filters for the convolutional layers.
-        depth (int, optional): Depth of the Xception block. Default is 4.
-        use_residual (bool, optional): Whether to use residual connections.
-            Default is True.
-        kernel_initialize (str, optional): Initializer for the kernel weights.
-        kernel_regularize (str, optional): Regularizer for the kernel weights.
-        kernel_constraint (str, optional): Constraint for the kernel weights.
+        inputs: Input tensor to the block.
+        n_filters: Number of filters for the convolutional layers.
+        depth: Depth of the Xception block.
+        use_residual: Whether to use residual connections.
+        kernel_initialize: The variance scaling initializer.
+        kernel_regularize: Penalty to apply on the layer's kernel.
+        kernel_constraint: The constraint of the value of the incoming weights.
 
     Returns:
-        x (tensor): Output tensor after applying the Xception block.
+        x: Output tensor after applying the Xception block.
     """
 
     x = inputs
@@ -274,36 +277,38 @@ def xception_block(inputs, n_filters, depth=4, use_residual=True,
     return x
 
 
-def xception_module(inputs, n_filters, use_bottleneck=True, kernel_size=41,
-                    stride=1, kernel_initialize=None, kernel_regularize=None,
-                    kernel_constraint=None):
-
+def xception_module(
+    inputs: tf.Tensor,
+    n_filters: int,
+    use_bottleneck: bool = True,
+    kernel_size: int = 41,
+    stride: int = 1,
+    kernel_initialize: str = None,
+    kernel_regularize: float = None,
+    kernel_constraint: int = None
+):
     """XceptionModule function that applies a series of SeparableConv1D layers
         and a MaxPooling1D layer, followed by concatenation.
 
     Args:
-        inputs (tensor): A Keras tensor serving as the starting point for this
-            module.
-        n_filters (int): Number of filters for the convolutional layers.
-        use_bottleneck (bool): Whether to use a bottleneck Conv1D layer at the
-            beginning. Default is True.
-        kernel_size (int): Kernel size for the SeparableConv1D layers.
-            Default is 41.
-        stride (int): Stride for the SeparableConv1D and MaxPooling1D layers.
-            Default is 1.
-        kernel_initialize (str): Initializer for the kernel weights.
-            Default is None.
-        kernel_regularize (str): Regularizer for the kernel weights.
-            Default is None.
-        kernel_constraint (str): Constraint for the kernel weights. Default is
-            None.
+        inputs: A Keras tensor serving as the starting point for this module.
+        n_filters: Number of filters for the convolutional layers.
+        use_bottleneck: Whether to use a bottleneck Conv1D layer at the
+            beginning.
+        kernel_size: Kernel size for the SeparableConv1D layers.
+        stride: Stride for the SeparableConv1D and MaxPooling1D layers.
+        kernel_initialize: Initializer for the kernel weights.
+        kernel_regularize: Regularizer for the kernel weights.
+        kernel_constraint: Constraint for the kernel weights.
 
     Returns:
-        x_post (tensor): Output tensor after applying the Xception module.
+        x_post: Output tensor after applying the Xception module.
     """
 
     if use_bottleneck and n_filters > 1:
-        x = Conv1D(filters=n_filters, kernel_size=1, padding="valid",
+        x = Conv1D(filters=n_filters,
+                   kernel_size=1,
+                   padding="valid",
                    use_bias=False,
                    kernel_initializer=kernel_initialize,
                    kernel_regularizer=kernel_regularize,
@@ -313,7 +318,7 @@ def xception_module(inputs, n_filters, use_bottleneck=True, kernel_size=41,
         x = inputs
 
     # Assuming kernel_padding_size_lists function is defined
-    kernel_sizes, padding_sizes = kernel_padding_size_lists(kernel_size)
+    kernel_sizes, _ = kernel_padding_size_lists(kernel_size)
 
     separable_conv_list = []
     for kernel in kernel_sizes:
@@ -340,12 +345,22 @@ def xception_module(inputs, n_filters, use_bottleneck=True, kernel_size=41,
     return x_post
 
 
-def kernel_padding_size_lists(max_kernel_size):
-    """
+def kernel_padding_size_lists(max_kernel_size: int):
+    """Generates lists of kernel sizes and corresponding paddings based on a
+        given max kernel size.
+
+    This function is designed to create lists of kernel sizes and their
+    corresponding paddings suitable for constructing convolutional neural
+    networks. The kernel sizes are determined by dividing the `max_kernel_size`
+    by powers of 2, and the paddings are calculated accordingly.
+
     Args:
-        max_kernel_size (int):
+        max_kernel_size: The maximum kernel size used as the reference for
+        generating smaller kernel sizes and corresponding paddings.
 
     Returns:
+        A tuple containing two lists - the first list contains generated kernel
+        sizes, and the second list contains corresponding paddings.
     """
     i = 0
     kernel_size_list = []
