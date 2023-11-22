58d417c833fb9dc847c3b04c5d13e79990b3fdd6
AIoT01 <tzamalis@ceid.upatras.gr>
2023-11-22 12:14:12 +0000

    version 0.1.1.7.4

--- crossai/__init__.py
+++ crossai/__init__.py
@@ -6,7 +6,7 @@ and developing end-to-end Artificial Intelligence pipelines for Time Series
 Analysis.
 """
 
-__version__ = "0.1.1.7.3"
+__version__ = "0.1.1.7.4"
 __author__ = ("Pantelis Tzamalis, Panagiotis Kapetanidis, Fotios Kalioras, "
               "George Kontogiannis, John Stamatelos, Andreas Bardoutsos")
 __credits__ = ("Academia and enterprise individuals who are AI and Time "
